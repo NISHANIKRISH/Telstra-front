70b127ce17a2d3e98d07b653bdea8ff22b9a705a
suhas <suhasmjnv17@gmail.com>
2023-04-04 21:25:56 +0530

    category page api linked with backend

--- src/containers/ProductComponent.js
+++ src/containers/ProductComponent.js
@@ -6,7 +6,7 @@ import { useSelector } from "react-redux";
 const ProductsList = () => {
   const products = useSelector((state) => state.allProducts.products);
   const renderList = products.map(function c(product) {
-    const { p_id, title, image, price, category } = product;
+    const { p_id, brand, name, image, price } = product;
     return (
 
       <div className="four wide column" key={p_id}>
@@ -14,12 +14,12 @@ const ProductsList = () => {
           <div className="ui link cards">
             <div className="card">
               <div className="image">
-                <img src={image} alt={title} />
+                <img src={image} alt={name} />
               </div>
               <div className="content">
-                <div className="header">{title}</div>
-                <div className="meta price">$ {price}</div>
-                <div className="meta">{category}</div>
+                <div className="header">{name}</div>
+                <div className="meta price">{price}</div>
+                <div className="meta">{brand}</div>
                 
               </div>
             </div>
