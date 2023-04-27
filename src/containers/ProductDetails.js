638a41b38faa37e60e387d59ebd8f3f9329d5c35
unknown <nishanikrishnappa02@gmail.com>
2023-04-27 23:50:37 +0530

    review ui design is done

--- src/containers/ProductDetails.js
+++ src/containers/ProductDetails.js
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+import { Container } from "react-bootstrap";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
 import { useParams } from "react-router-dom";
@@ -13,8 +14,29 @@ const ProductDetails = () => {
   
   const selected_products = useSelector((state) => state.product);
   const firstproduct = selected_products[0];
-  const { brand, category, description, image, name, p_id, price} = firstproduct || { };
+  const { category, description, image, name, p_id, price} = firstproduct || { };
   const dispatch = useDispatch();
+  const [productID, setProductID] = useState('');
+  const [review, setReview] = useState('');
+  const [rating, setRating] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsSubmitted(true);
+    setProductID(productID)
+    const data = { p_id, review, rating };
+    fetch('http://localhost:5000/review', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(data)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  }
   
   const fetchProductDetail = async (productId) => {
     const response = await axios
@@ -24,6 +46,7 @@ const ProductDetails = () => {
       });
     dispatch(selectedProduct(response.data));
   };
+
 
   useEffect(() => {
     if (productId && productId !== "") fetchProductDetail(productId);
@@ -96,7 +119,72 @@ const ProductDetails = () => {
    
       
     )}
-    <div style={{marginTop:'40px',width:'100%'}}><Review></Review></div>
+    <div style={{marginTop: '40px', width: '100%'}}>
+  <Container>
+    <div className='card' style={{width: 'auto', height: '100%', boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', borderRadius: '10px',padding:'20px'}}>
+      <div className='card__body'>
+      
+        <form onSubmit={handleSubmit} style={{padding: '30px'}}>
+          <h2 style={{paddingBottom: '30px', borderBottom: '2px solid #ddd'}}>Add your Review Here</h2>
+          <div style={{display: 'flex', alignItems: 'center', paddingBottom: '30px'}}>
+            <label htmlFor="rating" style={{fontSize: '24px', color: 'black', marginRight: '20px'}}>Rating:</label>
+            <select
+              id="rating"
+              name="rating"
+              value={rating}
+              onChange={(e) => setRating(e.target.value)}
+              style={{width: '50%'}}
+            >
+              <option value="">--Select--</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+          </div>
+          <div style={{display: 'flex', flexDirection: 'column', paddingBottom: '30px'}}>
+            <label htmlFor="review" style={{fontSize: '24px', color: 'black', paddingBottom: '10px'}}>Review:</label>
+            <textarea
+              id="review"
+              name="review"
+              value={review}
+              onChange={(e) => setReview(e.target.value)}
+              style={{width: '100%', height: '150px', padding: '10px', borderRadius: '5px', border: '1px solid #ddd'}}
+            />
+          </div>
+          <button 
+  style={{ 
+    width: '15%', 
+    padding: '8px', 
+    borderRadius: '5px', 
+    fontSize: '16px', 
+    backgroundColor: '#4bb9e1', 
+    color: 'white', 
+    boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', 
+    border: 'none',
+    transition: 'all 0.2s ease-in-out',
+    cursor: 'pointer',
+  }}
+  onMouseEnter={(e) => {
+    e.target.style.backgroundColor = '#3da2c2';
+  }}
+  onMouseLeave={(e) => {
+    e.target.style.backgroundColor = '#4bb9e1';
+  }}
+>
+  Submit
+</button>
+
+
+        </form>
+        {isSubmitted && <p style={{color:"green",fontSize:'20px'}}>Thank you for your review!</p>}
+
+      </div>
+    </div>
+  </Container>
+</div>
+<div style={{marginTop: '40px', width: '100%'}}> <Review productId={productId}></Review> </div>
   </div>
   
   );
